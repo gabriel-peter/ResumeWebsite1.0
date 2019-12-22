a5c9c8bf75b676df78404c7e2273ec6119493dcc
gabriel-peter <peter.g@husky.neu.edu>
2019-12-22 00:50:48 -0800

    More work on radial/bar graphs, animations explored.

--- client/src/components/spotify_previewer/chart_constructor.js
+++ client/src/components/spotify_previewer/chart_constructor.js
@@ -1,40 +1,55 @@
 import React, { Component } from 'react';
-import {XYPlot, LineSeries, VerticalBarSeries, RadialChart} from 'react-vis';
+import {XYPlot, LineSeries, VerticalBarSeries,DiscreteColorLegend, RadialChart} from 'react-vis';
 import './react-vis-graph-style.css';
 // http://uber.github.io/react-vis/documentation
 
 class Chart_Constructor extends Component {
+    constructor(props) {
+        super(props)
+
+    }
   render() {
-    const data = [
-      {x: 0, y: 8},
-      {x: 1, y: 5},
-      {x: 2, y: 4},
-      {x: 3, y: 9},
-      {x: 4, y: 1},
-      {x: 5, y: 7},
-      {x: 6, y: 6},
-      {x: 7, y: 3},
-      {x: 8, y: 2},
-      {x: 9, y: 0}
-    ];
+      const data = this.props.data
+      console.log('from constructor', data)
     return (
-      <div className="">
+      <div>
+          {/* <XYPlot
+  width={300}
+  height={300}
+  getX={d => d[0]}
+  getY={d => d[1]}>
+  <LineSeries
+    color="red"
+    data={[
+      [1, 0],
+      [2, 1],
+      [3, 2]
+    ]}/>
+</XYPlot>
         <XYPlot height={300} width={300}
-              colorType="category"
-              colorDomain={[0, 1, 2]}
-              colorRange={['blue', 'yellow', 'red']}>
-            <VerticalBarSeries data={data} />
-        </XYPlot>
+              colorType="category">
+            <VerticalBarSeries 
+            data={data} 
+            labelsAboveChildren={true}/>
+         </XYPlot>
+         
         <XYPlot height={300} width={300}>
             <LineSeries data={data} />
-        </XYPlot>
+        </XYPlot> */}
+
+        
         <RadialChart
-            data={[ {angle: 1, radius: 10}, 
-                {angle: 2, label: 'Super Custom label', subLabel: 'With annotation', radius: 20},
-                {angle: 5, radius: 5, label: 'Alt Label'}, {angle: 3, radius: 14}, 
-                {angle: 5, radius: 12, subLabel: 'Sub Label only', className: 'custom-class'} ]}
+            // data={[ {angle: 1, radius: 10}, 
+            //     {angle: 2, label: 'Super Custom label', subLabel: 'With annotation', radius: 20},
+            //     {angle: 5, radius: 5, label: 'Alt Label'}, {angle: 3, radius: 14}, 
+            //     {angle: 5, radius: 12, subLabel: 'Sub Label only', className: 'custom-class'} ]}
+            data={data}
             width={300}
-            height={300} />
+            height={300}
+            animation={{damping: 9, stiffness: 300}}
+
+            showLabels={true}/> 
+            {/* <DiscreteColorLegend/> */}
       </div>
     );
   }
--- client/src/components/spotify_previewer/spotify_previewer.js
+++ client/src/components/spotify_previewer/spotify_previewer.js
@@ -10,13 +10,18 @@ class Spotify_Previewer extends Component {
     constructor(){
         super();
         const params = this.getHashParams()
+        const top_artists_popularity = [{'x': 4, 'y': 2}];
         this.state = {
+            formattedResult: top_artists_popularity,
             access_token: '',
             loggedIn: params.access_token ? true : false,
-            nowPlaying: {
-                name: 'Not Checked',
-                image: ''
-            },
+            // nowPlaying: {
+            //     name: 'Not Checked',
+            //     image: ''
+            // },
+            short_term: {top_artists_popularity},
+            medium_term: {top_artists_popularity},
+            long_term: {top_artists_popularity},
         }
         if (params.access_token) {
             spotifyWebApi.setAccessToken(params.access_token);
@@ -25,19 +30,64 @@ class Spotify_Previewer extends Component {
             }));
         }
     }
+    piChartRankings(ranks) {
+        let keys = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90];
+        var values =[]
+        keys.forEach(e => {
+            var count = 0;
+            ranks.forEach(element => {
+                if(e === (Math.trunc((element-0.5)/10))*10) {
+                    count ++;
+                }
+            })
+            values.push(count)
+        })
+        // Radial Chart 
+        var formattedResult = [];
+        for (var i = 0; i <= values.length -1; i++) {
+            if(values[i] !== 0) {
+                formattedResult.push({'angle': values[i], 'label': 10*i, 'radius': i/2+5 })
+            }
+        }
+        // Bar Series
+        // var formattedResult = [];
+        // for (var i = 0; i <= values.length -1; i++) {
+        //     if(values[i] !== 0) {
+        //         formattedResult.push({'x': i, 'y': values[i],})
+        //     }
+        // }
+        
+        console.log('formatted pi Chart Data',formattedResult)
+        this.setState({
+            formattedResult
+        })
+    }
     analyseTermData(data, term) {
         const test_array = ["pop", "rap", "ssdsdsd"];
         const items = data.items;
         let top_artists = items.reduce((accumulator, currentValue) => accumulator.concat(currentValue.name), []);
+        // let top_artists_popularity = items.reduce((accumulator, currentValue) => accumulator.concat({'artist': currentValue.name, 'rank': currentValue.popularity}), []);
         let top_artists_popularity = items.reduce((accumulator, currentValue) => accumulator.concat(currentValue.popularity), []);
-        let average_artist_rank = top_artists_popularity.reduce((accumulator, currentValue) => accumulator + currentValue, 0)/50;
+        let average_artist_rank = items.reduce((accumulator, currentValue) => accumulator + currentValue.popularity, 0)/50;
         let genre_quantity = items.reduce((accumulator, currentValue) => accumulator.concat([currentValue.genres]), []).flat(); 
         const genre_intersection = test_array.filter(element => genre_quantity.includes(element));
-        this.setState({top_artists, top_artists_popularity, average_artist_rank, genre_quantity, genre_intersection,});
-        console.log('intersection', genre_intersection)
-        console.log(this.state.top_artists)
-        console.log(this.state.average_artist_rank);
-        console.log(this.state.genre_quantity);
+        if(term === 'short_term'){
+            this.setState({ short_term: {top_artists,
+                 top_artists_popularity, 
+                 average_artist_rank, genre_quantity, genre_intersection,}});
+        } else if(term === 'medium_term'){
+            this.setState({ medium_term: {top_artists, 
+                top_artists_popularity,
+                 average_artist_rank, genre_quantity, genre_intersection,}});
+        } else {
+            this.setState({ long_term: {top_artists, 
+                top_artists_popularity, 
+                average_artist_rank, genre_quantity, genre_intersection,}});
+            console.log(this.state.short_term.top_artists_popularity)
+            console.log(this.state.medium_term.average_artist_rank);
+            console.log(this.state.long_term.genre_quantity);
+            this.piChartRankings(top_artists_popularity)
+        }        
     }
     // getNowPlaying() {
     //     spotifyWebApi.getMyCurrentPlaybackState ()
@@ -51,20 +101,20 @@ class Spotify_Previewer extends Component {
     // }
     getTopArtists() {
         ['short_term', 'medium_term', 'long_term'].map(e => {
-        $.ajax({
-            url: `https://api.spotify.com/v1/me/top/artists?time_range=${e}&limit=50&offset=0`,
-            type: "GET",
-            beforeSend: (xhr) => {
-              xhr.setRequestHeader("Authorization", "Bearer " + this.getHashParams().access_token);
-            },
-            success: (data) => {
-              this.analyseTermData(data);
-            }
+            $.ajax({
+                url: `https://api.spotify.com/v1/me/top/artists?time_range=${e}&limit=50&offset=0`,
+                type: "GET",
+                beforeSend: (xhr) => {
+                xhr.setRequestHeader("Authorization", "Bearer " + this.getHashParams().access_token);
+                },
+                success: (data) => {
+                this.analyseTermData(data, e);
+                }
+            });
         });
-    });
         
     }
-    componentDidMount(){
+    componentWillMount(){
         if(this.state.loggedIn) {
             this.getTopArtists();
         }
@@ -97,11 +147,12 @@ class Spotify_Previewer extends Component {
                 <div>
                     <h3>{this.state.name}</h3>
                     <img src={this.state.image} alt='Album Image' height='auto' width='auto'/>
+                    <Chart_Constructor data={this.state.formattedResult}/>
                 </div>
                 )
                 } 
                 
-                <Chart_Constructor/>
+                
                 </div>
             );
     }
