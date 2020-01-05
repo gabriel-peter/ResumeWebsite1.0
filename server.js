89af4d3fe197949f1b13e42dca7e49edca7a93ac
gabriel-peter <peter.g@husky.neu.edu>
2020-01-05 13:47:34 -0500

    redirect error

--- server.js
+++ server.js
@@ -164,13 +164,13 @@ if (state === null || state !== storedState) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/spotify/#' +
+        res.redirect('https://resume-website-gabriel-peter.herokuapp.com/spotify/#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
           }));
       } else {
-        res.redirect('http://localhost:3000/spotify/#' +
+        res.redirect('https://resume-website-gabriel-peter.herokuapp.com/spotify/#' +
           querystring.stringify({
             error: 'invalid_token'
           }));
