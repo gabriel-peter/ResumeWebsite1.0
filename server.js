8a7fd90e37f28329906099e8380567cadf284b1f
gabriel-peter <peter.g@husky.neu.edu>
2020-01-05 22:12:04 -0500

    sf

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 const path = require('path');
 app.use(cors());
@@ -204,4 +204,4 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
