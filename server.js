05d3e27844450267b0f1b530c2040adc1c8d4ae1
gabriel-peter <peter.g@husky.neu.edu>
2020-01-05 20:14:02 -0500

    dfddf

--- server.js
+++ server.js
@@ -77,8 +77,8 @@ var cookieParser = require('cookie-parser');
 
 var client_id = '1bb626d08698445daef7e4dee1970679'; // Your client id
 var client_secret = '61b1bbfe4c094b6ba3fcb264b02c514c'; // Your secret
-// var redirect_uri = `http://localhost:5000/callback`; 
-var redirect_uri = `http://www.gabrielpeter.net/callback`; // Or Your redirect uri
+var redirect_uri = `http://localhost:5000/callback`; 
+// var redirect_uri = `http://www.gabrielpeter.net/callback`; // Or Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
